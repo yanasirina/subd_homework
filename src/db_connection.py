e38b0adc469a5ad9784d6d55f9630b682a9c505a
yanasirina <yanasirina0@yandex.ru>
2024-11-24 16:55:27 +0300

    ddl

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,17 +4,48 @@ import config
 
 
 class _DDL:
-    def __init__(self):
-        self.connection = DatabaseConnector().connection
+    def __init__(self, connection: connection):
+        self.connection = connection
 
     def _create_service_table(self):
-        pass
+        query = """
+        CREATE TABLE IF NOT EXISTS service (
+            service_id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            execution_time INT DEFAULT NULL
+        );
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            self.connection.commit()
 
     def _create_atelier_table(self):
-        pass
+        query = """
+        CREATE TABLE IF NOT EXISTS atelier (
+            atelier_id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            address TEXT NOT NULL,
+            phone_number VARCHAR(20) NOT NULL
+        );
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            self.connection.commit()
 
     def _create_service_cost_in_atelier_table(self):
-        pass
+        query = """
+        CREATE TABLE IF NOT EXISTS service_cost_in_atelier (
+            service_id INT NOT NULL,
+            atelier_id INT NOT NULL,
+            cost NUMERIC(10, 2) NOT NULL,
+            PRIMARY KEY (service_id, atelier_id),
+            FOREIGN KEY (service_id) REFERENCES service(service_id) ON DELETE CASCADE,
+            FOREIGN KEY (atelier_id) REFERENCES atelier(atelier_id) ON DELETE CASCADE
+        );
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            self.connection.commit()
 
     def create_tables(self):
         self._create_service_table()
@@ -22,7 +53,7 @@ class _DDL:
         self._create_service_cost_in_atelier_table()
 
 
-class DatabaseConnector:
+class _DatabaseConnector:
     """Класс для реализации Singleton подключения к PostgreSQL."""
     _instance = None
     _connection = None
@@ -31,7 +62,6 @@ class DatabaseConnector:
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._connection = cls._create_connection()
-            _DDL().create_tables()
         return cls._instance
 
     @staticmethod
@@ -53,3 +83,16 @@ class DatabaseConnector:
         if self._connection:
             self._connection.close()
             self._connection = None
+
+
+def get_connection() -> connection:
+    return _DatabaseConnector().connection
+
+
+def create_tables() -> None:
+    connection = get_connection()
+    _DDL(connection).create_tables()
+
+
+if __name__ == '__main__':
+    create_tables()
